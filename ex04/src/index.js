1fdb8d057c89fcadecbcc01cf536e402af18d5f1
sejfudin <sejfudin.duranovic@gmail.com>
2021-03-11 23:12:55 +0100

    Added file index.js

---
+++ ex04/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    var myDecimal = 8.2;
+    var myDecimalE = 2.54e-3;
+    return {
+        myDecimal,
+        myDecimalE,
+    };
+}
+console.log(main());
+module.exportts = main;
