20606a959b8039bd5c77a7dfaf75e622e0d76c9d
sejfudin <sejfudin.duranovic@gmail.com>
2021-03-12 00:16:02 +0100

    Added file index.js

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    // Only change code below this line
+    var myFirstName = "Sejfudin";
+    var myLastName = "Duranovic";
+    var myHomeTown = "Buzim";
+
+    // Only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown,
+    };
+}
+console.log(main());
+module.exports = main;
