91c75074898821cc1e0d8728dfaeb35365d3524b
sejfudin <sejfudin.duranovic@gmail.com>
2021-03-12 01:23:44 +0100

    Added file index.js

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,10 +1,11 @@
 function main(numOne, numTwo) {
-
     var quotient;
     if (numTwo === 0) {
         quotient = "Divide by 0! Cannot divide by zero!";
     } else {
+        // Only change code below this line
         quotient = numOne / numTwo;
+        // Only change code above this line
     }
 
     return quotient;
